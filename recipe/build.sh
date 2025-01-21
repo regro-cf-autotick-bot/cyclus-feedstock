f68b80d6fd01688da812b415f12ffcd67183148e
H. Vetinari <h.vetinari@gmx.com>
2025-01-21 15:56:24 +1100

    set --deps-root

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,6 +17,7 @@ fi
 ${PYTHON} install.py \
   --prefix="${PREFIX}" \
   --cmake_prefix_path="${PREFIX}" \
+  --deps-root="${PREFIX}" \
   --allow-milps \
   ${skiprpath} \
   -DCMAKE_C_COMPILER=${GCC} \
