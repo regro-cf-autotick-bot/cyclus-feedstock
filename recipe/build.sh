3929fe8be042e41e818d9eb7b0d213833984ad2d
H. Vetinari <h.vetinari@gmx.com>
2025-01-21 15:01:34 +1100

    set CMAKE_PREFIX_PATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,7 +14,9 @@ fi
 
 # Install Cyclus
 #export VERBOSE=1
-${PYTHON} install.py --prefix="${PREFIX}" \
+${PYTHON} install.py \
+  --prefix="${PREFIX}" \
+  --cmake_prefix_path="${PREFIX}" \
   --allow-milps \
   ${skiprpath} \
   -DCMAKE_C_COMPILER=${GCC} \
